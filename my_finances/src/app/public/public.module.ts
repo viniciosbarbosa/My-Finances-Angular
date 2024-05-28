ea9bc80bddc618c52031c4aa15286171e718ae23
viniciosl <viniciosbarbosagit@gmail.com>
2024-05-27 22:50:45 -0300

    inicializando as rotas publicas

--- my_finances/src/app/public/public.module.ts
+++ my_finances/src/app/public/public.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { PublicRoutes } from './public.routing';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, PublicRoutes],
 })
 export class PublicModule {}
--- my_finances/src/app/public/public.routing.ts
+++ my_finances/src/app/public/public.routing.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 ];
 
 export const PublicRoutes = RouterModule.forChild(routes);
