e152cf80c4bad5414cd290a35540e39ca70f853e
viniciosl <viniciosbarbosagit@gmail.com>
2024-05-27 22:52:16 -0300

    corrigindo bug da rota

--- my_finances/src/app/public/public.routing.ts
+++ my_finances/src/app/public/public.routing.ts
@@ -1,9 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-];
+const routes: Routes = [{ path: '', component: LoginComponent }];
 
 export const PublicRoutes = RouterModule.forChild(routes);
