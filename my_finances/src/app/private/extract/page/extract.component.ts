4d091edd3a75cffd3aa2e5e74e089a01a9d25cc8
viniciosl <viniciosbarbosagit@gmail.com>
2024-05-29 18:51:55 -0300

    extract okay

--- my_finances/src/app/private/extract/page/extract.component.ts
+++ my_finances/src/app/private/extract/page/extract.component.ts
@@ -2,6 +2,7 @@ import { ExtractService } from './../service/extract.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { dataExtractInfo } from '../model/dataExtractInfo';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-extract',
@@ -11,37 +12,117 @@ import { dataExtractInfo } from '../model/dataExtractInfo';
 export class ExtractComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
 
-  constructor(private extractService: ExtractService) {}
-
-  ngOnInit(): void {
-    this.getAllExtractValues();
-    console.log(typeof this.infoExtract.balance);
-  }
-
   infoExtract = {
     balance: 0,
     debts: 0,
     incomes: 0,
   };
 
+  btnGeralExtract: boolean = false;
+
+  months: Array<any> = [];
+  years: Array<any> = [];
+
+  formExtract!: FormGroup;
+
+  constructor(
+    private extractService: ExtractService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formInit();
+    this.getDatesYear();
+    this.getAllExtractValues();
+  }
+
   getAllExtractValues() {
     this.extractService
       .getAllExtract()
       .pipe(takeUntil(this.destroy$))
       .subscribe((response) => {
-        console.log(response);
         this.allBalances(response);
       });
   }
 
-  allBalances(balances: any) {
-    balances.forEach((balance: dataExtractInfo) => {
-      if (balance.tipo === 'receita') {
-        this.infoExtract.incomes += parseInt(balance.valor);
-      } else {
-        this.infoExtract.debts += parseInt(balance.valor);
-      }
+  clearFilterDatas() {
+    this.btnGeralExtract = false;
+    this.resetForm();
+    this.getAllExtractValues();
+  }
+
+  formInit(): void {
+    this.formExtract = this.formBuilder.group({
+      month: ['', Validators.required],
+      year: ['', Validators.required],
     });
+  }
+
+  async getDatesYear() {
+    await this.extractService
+      .getAllMouths()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response) => {
+        this.months = response;
+      });
+
+    await this.extractService
+      .getAllYears()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response) => {
+        this.years = response;
+      });
+  }
+  getInfoByMouthYear(): void {
+    const params = {
+      month: this.formExtract.value.month,
+      year: this.formExtract.value.year,
+    };
+
+    this.extractService
+      .getExtractByMouthYear(params)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.allBalances(response);
+            this.btnGeralExtract = true;
+          } else {
+            this.infoExtract.incomes = 0;
+            this.infoExtract.debts = 0;
+            this.infoExtract.balance = 0;
+          }
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  resetForm() {
+    this.formExtract.reset(
+      {
+        month: [''],
+        year: [''],
+      },
+      { emitEvent: false }
+    );
+  }
+
+  allBalances(balances: any) {
+    if (balances.length > 0) {
+      this.infoExtract.incomes = 0;
+      this.infoExtract.debts = 0;
+      this.infoExtract.balance = 0;
+
+      balances.forEach((balance: dataExtractInfo) => {
+        if (balance.tipo === 'receita') {
+          this.infoExtract.incomes += parseInt(balance.valor);
+        } else {
+          this.infoExtract.debts += parseInt(balance.valor);
+        }
+      });
+    }
 
     return (this.infoExtract.balance =
       this.infoExtract.incomes - this.infoExtract.debts);
--- my_finances/src/app/private/extract/service/extract.service.ts
+++ my_finances/src/app/private/extract/service/extract.service.ts
@@ -1,6 +1,8 @@
+import { FormsModule } from '@angular/forms';
 import { Injector, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpBaseService } from 'src/app/shared/service/http-base.service';
+import { dataByMonthYear } from '../model/dataByMouthYear';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +16,30 @@ export class ExtractService extends HttpBaseService {
 
   getAllExtract(): Observable<any> {
     return this.httpGet(this.endpoint);
+  }
+
+  getExtractByMouthYear(params: dataByMonthYear): Observable<any> {
+    const dataFiltered: Array<any> = [];
+
+    return this.httpGet(
+      `${this.endpoint}?mouth=${params.month}&year=${params.year}`
+    ).pipe(
+      map((data) => {
+        const dataFiltered = data.filter((item: any) => {
+          const [day, month, year] = item.data.split('/');
+          return month === params.month && year === String(params.year);
+        });
+
+        return dataFiltered;
+      })
+    );
+  }
+
+  getAllMouths(): Observable<any> {
+    return this.httpGet('meses');
+  }
+
+  getAllYears(): Observable<any> {
+    return this.httpGet('anos');
   }
 }
