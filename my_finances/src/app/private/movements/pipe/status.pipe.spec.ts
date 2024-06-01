af6c08953db157aec94a5999992ca61d2db00ceb
viniciosl <viniciosbarbosagit@gmail.com>
2024-06-01 12:15:28 -0300

    tdd components pages e pipe

--- my_finances/src/app/private/movements/pipe/status.pipe.spec.ts
+++ my_finances/src/app/private/movements/pipe/status.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { StatusPipe } from './status.pipe';
 
 describe('StatusPipe', () => {
+  let pipe: StatusPipe;
+
+  beforeEach(() => {
+    pipe = new StatusPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new StatusPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return "Pago" for truthy values', () => {
+    expect(pipe.transform(true)).toBe('Pago');
+    expect(pipe.transform(1)).toBe('Pago');
+    expect(pipe.transform('any string')).toBe('Pago');
+    expect(pipe.transform([])).toBe('Pago');
+    expect(pipe.transform({})).toBe('Pago');
+  });
+
+  it('should return "Pendente" for falsy values', () => {
+    expect(pipe.transform(false)).toBe('Pendente');
+    expect(pipe.transform(0)).toBe('Pendente');
+    expect(pipe.transform('')).toBe('Pendente');
+    expect(pipe.transform(null)).toBe('Pendente');
+    expect(pipe.transform(undefined)).toBe('Pendente');
   });
 });
